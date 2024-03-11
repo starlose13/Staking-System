2c018b57c7137b872038b2eae88b7725c3cab9e4
Pouya <jeopard13@yahoo.com>
2024-03-11 13:19:12 +0330

    trace the bugs and codes with lable

--- test/StakingContract.t.sol
+++ test/StakingContract.t.sol
@@ -4,21 +4,20 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {StakingContract} from "../src/StakingContract.sol";
 import {MockERC20} from "../src/Mocks.sol";
-
-
-contract StakingTest is Test , MockERC20 {
+// run the test with forge test -vvvv
+contract StakingTest is Test, MockERC20 {
     StakingContract public staking;
     MockERC20 public token;
     uint public constant AMOUNT = 1e10;
     function setUp() public {
         staking = new StakingContract();
-        token = new MockERC20();        
+        token = new MockERC20();
     }
     function testStakeFuzz(uint32 amount) public {
-        token.approve(address(staking),amount);
+        token.approve(address(staking), amount);
         bool success = staking.stake(amount, address(token));
         uint value = token.balanceOf(address(staking));
-        assertEq(value,amount);
+        assertEq(value, amount);
         assertTrue(success);
     }
 }
